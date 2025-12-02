fe86ab684498b0c6ce9068f07e6114326cb73b7a
Vishal <dubeyvishal714@gmail.com>
2025-12-02 17:24:21 +0530

    structure changed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import {registerValidator} from "../validators/userValidator.js"
+import {registerUser} from "../controllers/registerController.js"
+
 
 
 const router = express.Router();
 
-router.post("/register" ,  registerValidator , );
+router.post("/register" ,  registerValidator , registerUser );
 router.post("/login" , );
 router.get("/profile" , );
 router.get("/logout" , );
